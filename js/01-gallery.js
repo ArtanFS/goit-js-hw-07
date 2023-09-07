3871cc2f6f6731217854eb18aa48bd0f5f1e3726
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2023-09-07 23:28:38 +0300

    task01 step1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryList = galleryItems.map(({ preview, original, description }) => {
+    const item = document.createElement("li");
+    const itemLink = document.createElement("a");
+    const itemImg = document.createElement("img");
+
+    item.classList.add("gallery__item");
+    itemLink.classList.add("gallery__link");
+    itemLink.setAttribute("href", `${original}`);
+    itemImg.classList.add("gallery__image");
+    itemImg.setAttribute("src", `${preview}`);
+    itemImg.setAttribute("data-source", `${original}`);
+    itemImg.setAttribute("alt", `${description}`);
+
+    itemLink.append(itemImg);
+    item.append(itemLink);
+
+    return item;
+});
+
+gallery.append(...galleryList);
+
+// console.log(galleryItems);
