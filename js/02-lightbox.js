c23c9b28252af81317b1ab6fca334b49c8aa3cc5
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2023-09-10 19:36:09 +0300

    task02 step1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryList = galleryItems.map(({ preview, original, description }) => {
+    const item = document.createElement("li");
+    const itemLink = document.createElement("a");
+    const itemImg = document.createElement("img");
+
+    item.classList.add("gallery__item");
+    itemLink.classList.add("gallery__link");
+    itemLink.setAttribute("href", `${original}`);
+    itemImg.classList.add("gallery__image");
+    itemImg.setAttribute("src", `${preview}`);
+    itemImg.setAttribute("alt", `${description}`);
+
+    itemLink.append(itemImg);
+    item.append(itemLink);
+
+    return item;
+});
+
+gallery.append(...galleryList);
