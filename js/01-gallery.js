f97401817fe7d66b63d7854886d8fa87bd9ecb47
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2023-09-10 22:32:04 +0300

    fix code to pattern string

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,51 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
 
-const galleryList = galleryItems.map(({ preview, original, description }) => {
-    const item = document.createElement("li");
-    const itemLink = document.createElement("a");
-    const itemImg = document.createElement("img");
+const galleryList = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /></a></li>`
+  )
+  .join('');
 
-    item.classList.add("gallery__item");
-    itemLink.classList.add("gallery__link");
-    itemLink.setAttribute("href", `${original}`);
-    itemImg.classList.add("gallery__image");
-    itemImg.setAttribute("src", `${preview}`);
-    itemImg.setAttribute("data-source", `${original}`);
-    itemImg.setAttribute("alt", `${description}`);
-
-    itemLink.append(itemImg);
-    item.append(itemLink);
-
-    return item;
-});
-
-gallery.append(...galleryList);
-gallery.addEventListener("click", selectImage);
+gallery.innerHTML = galleryList;
+gallery.addEventListener('click', selectImage);
 
 function selectImage(event) {
-    event.preventDefault();
-    if (!event.target.classList.contains("gallery__image")) {
-        return;
-    }
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
 
-    const source = event.target.dataset.source;
-    const instance = basicLightbox.create(`<img src="${source}"> width="1280" height="853"`, {
-        onShow: (instance) => {
-            document.addEventListener("keydown", closeModal)
-        },
-        onClose: (instance) => {
-            document.removeEventListener("keydown", closeModal)
-        },
-        });
-    
-    instance.show()
-    
-    function closeModal(event) {
-        if (!basicLightbox.visible() || event.code !== "Escape") {
-            return;
-        }        
-        instance.close();
+  const source = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${source}"> width="1280" height="853"`,
+    {
+      onShow: instance => {
+        document.addEventListener('keydown', closeModal);
+      },
+      onClose: instance => {
+        document.removeEventListener('keydown', closeModal);
+      },
     }
+  );
+
+  instance.show();
+
+  function closeModal(event) {
+    if (!basicLightbox.visible() || event.code !== 'Escape') {
+      return;
+    }
+    instance.close();
+  }
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,32 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
 
-const galleryList = galleryItems.map(({ preview, original, description }) => {
-    const item = document.createElement("li");
-    const itemLink = document.createElement("a");
-    const itemImg = document.createElement("img");
+const galleryList = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`
+  )
+  .join('');
 
-    item.classList.add("gallery__item");
-    itemLink.classList.add("gallery__link");
-    itemLink.setAttribute("href", `${original}`);
-    itemImg.classList.add("gallery__image");
-    itemImg.setAttribute("src", `${preview}`);
-    itemImg.setAttribute("alt", `${description}`);
-
-    itemLink.append(itemImg);
-    item.append(itemLink);
-
-    return item;
-});
-
-gallery.append(...galleryList);
+gallery.innerHTML = galleryList;
 
 var lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: "alt",
-    captionDelay: 250,
-    closeText: "",
-    showCounter: false,
-    disableRightClick: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  closeText: '',
+  showCounter: false,
+  disableRightClick: true,
 });
